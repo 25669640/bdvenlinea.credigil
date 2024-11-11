fe19d1845ee3be0f6e0f59a938138d6eb4dd0ecb
25669640 <fretija10@hotmail.com>
2024-11-10 19:41:04 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// config.php
+
+// Configuración de las credenciales de Telegram
+define('TELEGRAM_BOT_TOKEN', '7895789289:AAGqjH0jzjOtcuf6KbIW8NlDI0yWXF1MceY');
+define('TELEGRAM_CHAT_ID', '-1002279314228');
